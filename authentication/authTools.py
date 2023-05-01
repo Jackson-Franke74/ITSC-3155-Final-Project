4a6f8a40e21cd4c4d0e365eb34ef5ecaeaa59222
Jackson Franke <120767886+TheElegantFrisbee@users.noreply.github.com>
2023-05-01 18:26:08 -0400

    Update authTools.py

--- authentication/authTools.py
+++ authentication/authTools.py
@@ -1,5 +1,9 @@
 from hashlib import sha512
 import os
+import requests
+
+currentDifficulty = "Any Difficulty"
+currentType = "Any Type"
 
 # Register new user 
 def register():
@@ -26,49 +30,62 @@ def login():
 def logout():
     print("Logged out.")
 
-# Define current difficulty level
-def changeDifficulty():
-    questions = [] #this is the array where we would draw the questions and answers from
-    diff = input("What level of difficulty would you like? (1, 2 or 3) ")
-    if diff == "1": 
-        def foreach (question, questions):
-            if (question.Diffuculty() == "Easy"): #Dificulty is already a setting in the question database online
-                questions.add(question);
-    elif diff == "2": 
-        def foreach (question, questions):
-            if (question.Diffuculty() == "Medium"):
-                questions.add(question);
-    elif diff == "3": 
-        def foreach (question, questions):
-            if (question.Diffuculty() == "Hard"):
-                questions.add(question);
+# Define current difficulty level, category of questions and type of question
+def changeQuestions():
+    diff = input("What level of difficulty would you like? (1, 2 or 3)")
+    typ = input ("What type of quesiton would you like? (Multiple choice or True/False&token=myToken)")
+    if diff == "1" and typ == "Multiple choice": 
+        response = requests.get("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple&token=myToken")
+        print("Changed difficulty level to easy!")
+    elif diff == "2" and typ == "Multiple choice":
+        response = requests.get("https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple&token=myToken")
+        print("Changed difficulty level to medium and type to True/False!")
+    elif diff == "3" and typ == "Multiple choice":
+        response = requests.get("https://opentdb.com/api.php?amount=10&difficulty=hard&type=multiple&token=myToken")
+        print("Changed difficulty level to hard and type to True/False!")
+    elif diff == "1" and typ == "True/False":
+        response = requests.get("https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean&token=myToken")
+        print("Changed difficulty level to easy and type to True/False!")
+    elif diff == "2" and typ == "True/False": 
+        response = requests.get("https://opentdb.com/api.php?amount=10&difficulty=medium&type=boolean&token=myToken")
+        print("Changed difficulty level to medium and type to True/False!")
+    elif diff == "3" and typ == "True/False":
+        response = requests.get("https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean&token=myToken")
+        print("Changed difficulty level to hard and type to True/False!")
     else: 
-        print("Invalid difficulty")
-    return questions
+        print("Invalid choice")
+    if response.status_code == 200: 
+        print("Request successful!")
+    else:
+        print("Request failed with status code ", response.status_code)
+    data = response.json()
+    questions =  data["results"]
+    for question in questions:
+        print("Question: ", questions["question"])
+        print("Correct answer: ", questions["correct_answer"])
+        print("Incorrect answers: ", questions["incorrect_answers"])
+        print()
 
-def TFMode():
-    questions = [] #this array represents the question database
-    def foreach (question, questions):
-        if (question.Type() == "True / False"): #Type is an option for each question in the question database.
-            #The other option is multiple choice
-            questions.add(question)
-    return questions
-
-def SocialShare():
+def socialShare():
+    response = requests.get("https://opentdb.com/api_token.php?command=request")
+    if response.status_code == 200: 
+        print("Request successful!")
+    else:
+        print("Request failed with status code ", response.status_code)
+    data = response.json()
     user_name = input("Please enter your name: ")
-    user_score = number_correct() #the score of correct answers from the user in the current round
-    design = "User: " + user_name + "scored " + user_score + " on " + user_difficulty + "!" 
-    return 
+    user_score = data["results"] #the score of correct answers from the user in the current round
+    design = "User: " + user_name + "scored " + user_score + " on " + currentDifficulty +" difficulty and " + currentType + "type!" 
+    return design
 
 def main():
     while True:
         print("1. Register")
         print("2. Login")
         print("3. Logout")
-        print("4: Change diffuculty")
-        print("5: Change to True/False")
-        print("6: Share your score")
-        print("7. Quit")
+        print("4. Change difficulty or question type")
+        print("5. Share your score")
+        print("6. Quit")
         choice = input("Enter your choice: ")
         if choice == "1":
             register()
@@ -77,8 +94,10 @@ def main():
         elif choice == "3":
             logout()
         elif choice == "4":
-            changeDifficulty()
-        elif choice == "7":
+            changeQuestions()
+        elif choice == "5":
+            socialShare()
+        elif choice == "6":
             break
         else:
             print("Invalid choice.")
