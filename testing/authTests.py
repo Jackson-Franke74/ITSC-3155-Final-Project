1a8a130db74f77b45f4bfc55fc499c1084f26e9f
Neel Dadhania <123699536+NeelKV@users.noreply.github.com>
2023-05-02 11:57:55 -0400

    Created test case for core

--- testing/authTests.py
+++ testing/authTests.py
@@ -1,38 +1,51 @@
-from authentication.authTools import *
+import unittest
 
+# class authTests(unittest.TestCase):
 
-def test_register():
-    name = "user1"
-    email = "user@email.com"
-    password = "password"
-    register.name = name
-    register.email = email
-    register.password = password
-    register()
-    with open("users.txt", "a") as f:
-        test_var = f.read()
-    test_array = test_var.split()
-    if name == test_array[0]:
-        print("Test Registration Name: Pass")
-    else:
-        print("Test Registration Name: Fail")
+#     def test_register(self):
 
-    if email == test_array[1]:
-        print("Test Registration Email: Pass")
-    else:
-        print("Test Registration Email: Fail")
-    if password == test_array[2]:
-        print("Test Registration Password: Pass")
-    else:
-        print("Test Registration Password: Fail")
+#         name = "user1"
+#         email = "user@email.com"
+#         password = "password"
+#         register.name = name
+#         register.email = email
+#         register.password = password
+#         register()
+#         with open("users.txt", "a") as f:
+#             test_var = f.read()
+#         test_array = test_var.split()
+#         if name == test_array[0]:
+#             print("Test Registration Name: Pass")
+#         else:
+#             print("Test Registration Name: Fail")
+
+#         if email == test_array[1]:
+#             print("Test Registration Email: Pass")
+#         else:
+#             print("Test Registration Email: Fail")
+#         if password == test_array[2]:
+#             print("Test Registration Password: Pass")
+#         else:
+#             print("Test Registration Password: Fail")
     
 
-def test_login():
-    email = "user@email.com"
-    password = "password"
-    register.email = email
-    register.password = password
-    register()
-    login.email = email
-    login.password = password
-    login()
+#     def test_login():
+#         email = "user@email.com"
+#         password = "password"
+#         register.email = email
+#         register.password = password
+#         register()
+#         login.email = email
+#         login.password = password
+#         login()
+
+#     def changeDIfficulty_test():
+#         questions_test = changeDifficulty()
+
+#     def TFMode_Test():
+#         TFquestions_test = TFMode()
+
+#     def SocialShare_test():
+#         design_test = SocialShare()
+
+
