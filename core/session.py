728d99088531436859a9e1d5f233aa58e3bd0dde
Anushrut Neupane <anushrutneupane@gmail.com>
2023-05-02 13:09:39 -0400

    classes b-gone

--- core/session.py
+++ core/session.py
@@ -3,92 +3,80 @@ import requests
 
 currentDifficulty = "Any Difficulty"
 currentType = "Any Type"
+    
+def create_points_system() -> dict:
+    return{}
 
-class PointSystem:
-    def __init__(self):
-        self.points = {}
-    
-    def add_points(self, user, points):
-        if user in self.points:
-            self.points[user] += points
-        else:
-            self.points[user] = points
-    
-    def remove_points(self, user, points):
-        if user in self.points:
-            self.points[user] -= points
-            if self.points[user] < 0:
-                self.points[user] = 0
+def add_points(points: dict, user: str, points_to_add: int):
+    if user in points:
+        points[user] += points_to_add
+    else:
+        points[user] = points_to_add
 
-    def get_points(self, user):
-        return self.points.get(user, 0)
+def remove_points(points: dict, user: str, points_to_remove: int):
+    if user in points:
+        points[user] -= points_to_remove
+        if points[user] < 0:
+            points[user] = 0
+
+def get_points(points: dict, user: str) -> int:
+    return points.get(user, 0)
+
     
-class TriviaQuestion:
-    def __init__(self, question: str, answers: List[str], correct_answer: str, point_value: int):
-        self.question = question
-        self.answers = answers
-        self.correct_answer = correct_answer
-        self.point_value = point_value
-    
-    @classmethod
-    def create_question(cls):
-        question = input("Please enter the trivia question: ")
-        answers = []
-        while True:
-            answer = input("Please enter the answers (or 'done' to finish): ")
-            if answer == 'done':
-                break
-            answers.append(answer)
+def create_question() -> dict:
+    question = input("Please enter your trivia question: ")
+    answers = []
+    while True:
+        answer = input("Please enter the answers (or 'done' to finish): ")
+        if answer == 'done':
+            break
+        answers.append(answer)
         correct_answer = input("Please enter the correct answer: ")
-        point_value = int(input("Please enter the point value: "))
-        return cls(question, answers, correct_answer, point_value)
+        point_value = input("Please enter the point value: ")
+        return{
+            'question' : question,
+            'answers' : answers,
+            'correct_answer' : correct_answer,
+            'point_value' : point_value
+        }
+    
+def check_answer(trivia_question: dict, player_answer: str) -> bool:
+    return player_answer.lower() == trivia_question['correct_answer'].lower()
+    
 
-    
-    def check_answer(self, player_answer: str) -> bool:
-        return player_answer.lower() == self.correct_answer.lower()
-    
-    
-class TriviaGame:
-    def __init__(self, questions: List[dict]):
-        self.questions = questions
-        self.players = {}
+def add_player(players: dict[str, int], name: str) -> None:
+    players[name] = 0
 
-    def add_player(self, name: str):
-        self.players[name] = 0
-    
-    def play_question(self, questions: dict, player: str, answer: str) -> bool:
-        q = TriviaQuestion(questions["question"], questions["answer"], questions["correct_answer"], questions["point_value"])
-        if answer.lower() == q.correct_answer.lower():
-            print("Correct!")
-            self.players[player] += q.point_value
-        else:
-            print("Incorrect!")
-    
-    def play_game(self):
-        for question in self.questions:
-            print("Question:", question["question"],"\nPoint Value:",question["point_value"])
-            for i, answer in enumerate(question["answer"]):
-                print(f"{i+1}. {answer}")
-            response = input("Your answer (put the number that's corresponding.): ")
-            player = input("Your name: ")
-            if player not in self.players:
-                self.add_player(player)
-            if response.isdigit():
-                response = int(response)
-                if response >= 1 and response <= len(question["answer"]):
-                    if self.play_question(question, player, question["answer"][response-1]):
-                        pass
-                    else:
-                        pass
-                else:
-                    print("invalid answer number")
+def play_question(question: dict[str, any], players: dict[str, int], player: str, answer: str) -> None:
+    if answer.lower() == question['correct_answer'].lower():
+        print("Correct!")
+    else:
+        print("Incorrect!")
+
+def play_game(questions: List[dict[str, any]]) -> dict[str, int]:
+    players = {}
+    for question in questions:
+        print("Question: ", question['question'], "\nPoint Value: ", question['point_value'])
+        for i, answer in enumerate(question['answer']):
+            print(f"{i+1}.{answer}")
+        response = input("Your answer (put the number that's corresponding.): ")
+        player = input("Your name: ")
+        if player not in players:
+            add_player(players, player)
+        if response.isdigit():
+            response = int(response)
+            if response >= 1 and response <= len(question['answer']):
+                play_question(question, players, question["answer"][response-1])
             else:
-                print("invalid input")
-    
-    def print_leaderboard(self):
-        sorted_players = sorted(self.players.items(), key= lambda x:x[1], reverse= True)
-        for player, score in sorted_players:
-            print(f"{player}: {score} points")
+                print("invalid answer number")
+        else:
+            print("invalid input")
+    return players
+
+def print_leaderboard(players: dict[str, int]) -> None:
+    sorted_players = sorted(players.items(), key= lambda x:x[1], reverse= True)
+    for player, score in sorted_players:
+        print(f"{player}: {score} points")
             
 # Define current difficulty level, category of questions and type of question
 def changeQuestions():
@@ -149,20 +137,3 @@ def socialShare():
     user_score = data["results"] #the score of correct answers from the user in the current round
     design = "User: " + user_name + "scored " + user_score + " on " + currentDifficulty +" difficulty and " + currentType + " type!" 
     return design
-
-
-def main():
-    makeOwnq1 = TriviaQuestion.create_question()
-
-    questins = [{"question": makeOwnq1.question,
-                 "answer" : makeOwnq1.answers,
-                 "correct_answer": makeOwnq1.correct_answer,
-                 "point_value": makeOwnq1.point_value}]
-
-    
-    game = TriviaGame(questins)
-    game.play_game()
-    game.print_leaderboard()
-
-if __name__ == "__main__":
-    main()
